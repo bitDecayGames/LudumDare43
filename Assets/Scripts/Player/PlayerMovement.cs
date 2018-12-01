2afc30781c001b35b8db8d2db9fef5e5ea6ed44c
mwingfield <mwingfie@gmail.com>
2018-12-01 02:55:01 -0700

    moved player script into its own folder

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Player {
+	public class PlayerMovement : MonoBehaviour
+	{
+		public float speed = 5;
+
+		private void FixedUpdate()
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+		}
+	}
+}
