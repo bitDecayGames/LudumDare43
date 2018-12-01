5b97e2707e153423ad017b5b9b27cf48c7ffb1de
Tanner <Shadoninja@gmail.com>
2018-12-01 15:52:03 -0700

    fixed again

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKeyDown("space"))
         {
             playerHands.GetComponent<Grabber>().Activate(gameObject);
         }
