bb98cf4ee0443577a962904fd26e72360b8815b1
Logan <monday.hopscotch@gmail.com>
2018-12-02 12:48:56 -0700

    add cargo art to tile map

--- Assets/Prefabs/Levels/Cargo.tsx
+++ Assets/Prefabs/Levels/Cargo.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Cargo" tilewidth="48" tileheight="64" tilecount="5" columns="0">
+<tileset version="1.2" tiledversion="1.2.1" name="Cargo" tilewidth="80" tileheight="112" tilecount="29" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
@@ -15,7 +15,7 @@
  </tile>
  <tile id="1">
   <properties>
-   <property name="delay" type="float" value="15"/>
+   <property name="delay" type="float" value="10"/>
    <property name="score" type="float" value="2"/>
   </properties>
   <image width="36" height="52" source="../../Images/Pieces/l.png"/>
@@ -27,7 +27,7 @@
  </tile>
  <tile id="2">
   <properties>
-   <property name="delay" type="float" value="3"/>
+   <property name="delay" type="float" value="6"/>
    <property name="score" type="float" value="2"/>
   </properties>
   <image width="48" height="32" source="../../Images/Pieces/s.png"/>
@@ -58,6 +58,298 @@
   <objectgroup draworder="index">
    <object id="14" x="0" y="2.36364">
     <polygon points="0,0 2.18182,-2.27273 13,-2.36364 15.8182,0.545455 15.8182,13.7273 29.3636,13.7273 31.9091,15.6364 31.8182,27.8182 29.9091,29.5455 15.9091,29.5455 15.8182,43.6364 13.7273,45.5455 1.81818,45.6364 0,43.6364"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/1.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="3.09091">
+    <polygon points="0,0 3.09091,-3.09091 45.1818,-3.09091 48,-0.727273 48,10.6364 46,12.7273 16,12.7273 15.8182,26.5455 14.0909,28.8182 2.27273,28.8182 0,27.3636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/2.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="18.9091" y="0.181818">
+    <polygon points="0,0 -2.90909,2.90909 -3.09091,15.7273 -16.8182,15.7273 -18.8182,18.0909 -18.8182,30 -17.3636,31.7273 10.7273,31.7273 12.8182,29.7273 12.9091,15.4545 26.7273,15.5455 28.9091,13.5455 29.0909,1.90909 27.5455,-0.272727"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/3.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="39.8182" y="3.27273">
+    <polygon points="0,0 -2.36364,-3.36364 -34.1818,-3.36364 -39.7273,0.727273 -39.8182,7.54545 -35.2727,11.6364 -22.6364,11.6364 -22.7273,22.9091 -18.7273,28.3636 -2.36364,28.4545 0,26"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/4.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="3.27273">
+    <polygon points="0,0 3,-3.27273 12.5455,-3.36364 15.9091,-0.636364 15.8182,12.8182 45.8182,12.8182 47.8182,14.2727 48,27.1818 46.2727,28.8182 2.81818,28.7273 0.0909091,27"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/5.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="18.6364" y="0">
+    <polygon points="0,0 -2.63636,2.72727 -2.54545,16 -16.6364,16 -18.5455,18.2727 -18.5455,30.2727 -16.7273,31.9091 11.0909,31.9091 13.0909,30.3636 13.1818,15.7273 26.8182,15.8182 29.3636,13.4545 29.3636,1.90909 27.2727,-0.0909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/6.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="15.8182">
+    <polygon points="0,2.27273 0,14.0909 2.18182,16.0909 45.3636,16.0909 47.8182,13.6364 47.8182,1.63636 45.0909,-0.0909091 31.9091,0 31.8182,-14 29.7273,-15.8182 18,-15.8182 15.9091,-13.7273 15.8182,0.0909091 2,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/7.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="3.18182" y="15.8182">
+    <polygon points="0,0 -3.09091,4 -3.27273,12.8182 0.363636,16.0909 40.7273,16.1818 44.7273,12.2727 44.8182,3.54545 41.1818,-0.0909091 27.7273,-0.181818 27.7273,-13.0909 25.0909,-15.7273 16.0909,-15.7273 13,-12.1818 12.7273,0.0909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/8.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="2.54545" y="0">
+    <polygon points="0,0 -2.54545,2.27273 -2.54545,29.4545 -0.0909091,32 26.9091,32 28.9091,29.9091 28.5455,20.9091 43.0909,20.9091 45.3636,19.6364 45.4545,12 43.4545,10.5455 28.5455,10.7273 28.4545,1.36364 26.6364,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="16" height="64" source="../../Images/Pieces/9.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="2.36364">
+    <polygon points="0,0 2,-2.45455 14,-2.45455 16.0909,-0.363636 16.1818,59.1818 13.7273,61.5455 2.54545,61.5455 0.181818,59.5455"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="16" height="64" source="../../Images/Pieces/10.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="2.72727">
+    <polygon points="0,0 2.18182,-2.63636 14.0909,-2.63636 16,-0.454545 16.1818,58.8182 14.2727,61.1818 2.54545,61.1818 0.363636,59.1818"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="32" height="32" source="../../Images/Pieces/11.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="2.63636">
+    <polygon points="0,0 2.63636,-2.54545 29.4545,-2.54545 31.7273,-0.272727 31.9091,27.3636 29.7273,29.2727 2.09091,29.2727 0,27.3636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="32" height="32" source="../../Images/Pieces/12.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="2.63636">
+    <polygon points="0,0 2.09091,-2.63636 29.5455,-2.54545 32.0909,-0.818182 32,27.4545 30.2727,29.1818 2.18182,29.2727 0,27.3636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="32" height="32" source="../../Images/Pieces/13.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="12.5455">
+    <polygon points="0.0909091,1.27273 2.54545,3.36364 15.9091,3.45455 15.9091,17.7273 17.5455,19.3636 30.6364,19.3636 32,17.4545 32,-4.90909 25,-12.4545 2.18182,-12.5455 0.0909091,-10.7273"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="16" height="32" source="../../Images/Pieces/14.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="6.81818">
+    <polygon points="0,0 3.36364,-6.90909 13,-6.81818 16.0909,0 16.0909,21.0909 14,25.0909 3,25 0.272727,21.8182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="16" height="32" source="../../Images/Pieces/15.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="4.36364">
+    <polygon points="0,0 4.09091,-4.54545 11.8182,-4.45455 16,0.363636 15.9091,22.5455 12.5455,27.6364 2.81818,27.5455 0,21.5455"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="16" height="16" source="../../Images/Pieces/16.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="3.27273">
+    <polygon points="0,0 3.18182,-3.54545 13,-3.27273 16,-0.181818 16.1818,9.72727 13.0909,12.5455 3.27273,12.6364 0.0909091,9.63636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="16" height="16" source="../../Images/Pieces/17.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="1.54545">
+    <polygon points="0,0 1.27273,-1.63636 14.0909,-1.54545 15.9091,0 15.9091,13 13.8182,14.5455 1.63636,14.3636 0,12.3636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="16" source="../../Images/Pieces/18.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="1.90909">
+    <polygon points="0,0 1.81818,-1.90909 37.9091,-1.90909 39.9091,-0.454545 40,12.7273 38.5455,14.0909 2.27273,14.0909 0.0909091,12.7273"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="64" source="../../Images/Pieces/19.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="18.4545" y="15.1818">
+    <polygon points="0,0 0.0909091,-13.3636 2.18182,-15.1818 8.72727,-15.1818 10.9091,-12.2727 11.5455,0.181818 27.6364,0.272727 29.5455,1.36364 29.5455,7.36364 28.1818,8.81818 13.5455,8.90909 13.6364,46.9091 11.5455,48.8182 -0.454545,48.8182 -2.72727,47.2727 -2.63636,8.63636 -16.3636,8.63636 -18.3636,7.36364 -18.5455,1.63636 -16.2727,0.454545"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="48" source="../../Images/Pieces/20.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="16" y="1.72727">
+    <polygon points="0,0 1.63636,-1.63636 13.7273,-1.81818 15.8182,-0.0909091 15.8182,14.2727 22.4545,14.2727 23.8182,16 23.9091,36.5455 22.6364,38 16,38 15.8182,44.6364 14.3636,46.0909 -12.6364,46.0909 -15.9091,43.3636 -16,32.6364 -12.7273,30.0909 0,30"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="32" height="112" source="../../Images/Pieces/21.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.363636" y="22.0909">
+    <polygon points="0,0 12.3636,-22.1818 17.9091,-22.1818 31.4545,-1.27273 31.4545,87 29.2727,89.7273 2.72727,89.8182 -0.363636,87.3636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="16" height="32" source="../../Images/Pieces/22.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="2.27273">
+    <polygon points="0,0 1.81818,-2.27273 14.4545,-2.36364 16.1818,-0.363636 16.0909,28 14.4545,29.7273 1.90909,29.8182 0.0909091,28"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="80" height="32" source="../../Images/Pieces/23.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="3">
+    <polygon points="0,0 -0.0909091,-2.09091 1.09091,-3 77.8182,-2.90909 79.9091,-1.81818 79.8182,-0.0909091 58.0909,0.181818 58.3636,18.5455 80,18.7273 80,20.3636 78.9091,21 2.09091,20.9091 -0.0909091,20 -0.0909091,18.7273 21,18.7273 20.8182,-0.181818"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="delay" type="float" value="20"/>
+   <property name="score" type="float" value="2"/>
+  </properties>
+  <image width="48" height="32" source="../../Images/Pieces/24.png"/>
+  <objectgroup draworder="index">
+   <object id="2" x="3.72727" y="15.7273">
+    <properties>
+     <property name="delay" type="float" value="10"/>
+     <property name="score" type="float" value="5"/>
+    </properties>
+    <polygon points="0,0 2.81818,0 13.5455,4.45455 28.6364,4.72727 37.7273,-6.81818 38,-14.3636 39.5455,-15.9091 42.4545,-15.8182 44.2727,-14.0909 44.1818,6.45455 35,16 11.2727,16.2727 -3.81818,5.18182 -3.81818,1.09091"/>
    </object>
   </objectgroup>
  </tile>
