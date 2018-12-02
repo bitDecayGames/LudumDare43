791a9d0105c5c0e9cb08bae6bd1c75717275a7f4
Tanner <Shadoninja@gmail.com>
2018-12-02 13:50:45 -0700

    sfx added

---
+++ Assets/Scripts/Constants/Sfx.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Sfx : MonoBehaviour
+{
+    public static string AmbientFogHorn = "SFX/Ambient/FogHorn ";
+    public static string AmbientShipBell = "SFX/Ambient/ShipBell ";
+    public static string ImpactMetal = "SFX/Impact/Metal ";
+    public static string ImpactMetal2 = "SFX/Impact/Metal2 ";
+    public static string ImpactMetalBarrel = "SFX/Impact/MetalBarrel ";
+    public static string ImpactMetalBarrel2 = "SFX/Impact/MetalBarrel2 ";
+    public static string ImpactWood = "SFX/Impact/Wood ";
+    public static string ImpactWood2 = "SFX/Impact/Wood2";
+    public static string MenuMove = "SFX/Menu/MenuMove";
+    public static string MenuSelect = "SFX/Menu/Select";
+    public static string StepWood = "SFX/Step/Wood";
+}
