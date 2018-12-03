589591ae78ffd6b76231e4916a1e25e85fcd0b1d
mwingfield <mwingfie@gmail.com>
2018-12-03 14:38:50 -0700

    fixed tanner's wierd commit

--- Assets/Scripts/TrashZone/TrashZoneBehaviour.cs
+++ Assets/Scripts/TrashZone/TrashZoneBehaviour.cs
@@ -4,47 +4,6 @@ using SuperTiled2Unity;
 using UnityEngine;
 
 namespace TrashZone {
-<<<<<<< HEAD
-	public class TrashZoneBehaviour : MonoBehaviour {
-
-		public SpriteRenderer TrashZoneShader;
-		private Material zoneMat;
-
-		private BoxCollider2D myBox;
-
-		private CargoBehaviour cargo;
-		private Action<CargoBehaviour> onDrop;
-
-		private float lineOffset;
-
-		void Start() {
-			zoneMat = TrashZoneShader.material;
-			myBox = GetComponent<BoxCollider2D>();
-		}
-		
-		void Update() {
-			lineOffset += Time.deltaTime * 2;
-			SetDashOffset();
-		}
-
-		private void SetDashOffset() {
-			if (zoneMat != null) {
-				zoneMat.SetFloat("_LineOffset", lineOffset);
-			}
-		}
-
-		public void CheckAndTakeOutTrash(Transform cargo) {
-			print("Check and take out trash! " + cargo.position);
-			if (myBox.OverlapPoint(cargo.position))
-			{
-				FMODSoundEffectsPlayer.GetLocalReferenceInScene().PlaySoundEffect(Sfx.AmbientItemFallingOffShip);
-				var superObj = cargo.gameObject.GetComponentInAncestor<SuperObject>();
-				superObj.GetComponent<CargoBehaviour>().DestroyInWater();
-			}
-		}
-	}
-}
-=======
     public class TrashZoneBehaviour : MonoBehaviour {
         public SpriteRenderer TrashZoneShader;
         private Material zoneMat;
@@ -75,6 +34,8 @@ namespace TrashZone {
         public void CheckAndTakeOutTrash(Transform cargo) {
             print("Check and take out trash! " + cargo.position);
             if (myBox.OverlapPoint(cargo.position)) {
+                FMODSoundEffectsPlayer.GetLocalReferenceInScene().PlaySoundEffect(Sfx.AmbientItemFallingOffShip);
+
                 var superObj = cargo.gameObject.GetComponentInAncestor<SuperObject>();
                 var cargoObj = superObj.GetComponent<CargoBehaviour>();
                 cargoObj.DestroyInWater();
@@ -83,4 +44,3 @@ namespace TrashZone {
         }
     }
 }
->>>>>>> d8029b5589383a960fa7d6072de986c663422f13
