a875e3d9386492c6bc41c5593651b4b81ea894bd
mwingfield <mwingfie@gmail.com>
2018-12-02 22:00:09 -0700

    add finish reason text

--- Assets/Scripts/Cargo/CargoBehaviour.cs
+++ Assets/Scripts/Cargo/CargoBehaviour.cs
@@ -19,6 +19,9 @@ namespace Cargo {
         public int score = 1;
         public bool isBonus;
         public float delay;
+        public string name;
+        public string description;
+        public string bonusDescription;
 
         void Start() {
             var level = FindObjectOfType<LevelBehaviour>();
@@ -39,6 +42,7 @@ namespace Cargo {
                     Destroy(player.gameObject);
                     var level = FindObjectOfType<LevelBehaviour>();
                     if (level != null) {
+                        level.SetFinishReasonText("You Got Squished!");
                         level.Finished();
                     }
                 }
@@ -61,6 +65,7 @@ namespace Cargo {
                             DestroyOnBoat();
                             var level = FindObjectOfType<LevelBehaviour>();
                             if (level != null) {
+                                level.SetFinishReasonText("You Broke The Crane!");
                                 level.Finished();
                                 return;
                             }
