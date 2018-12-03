f326d41165e51d01ac4ad82988b4b78891541b1c
mwingfield <mwingfie@gmail.com>
2018-12-03 16:32:42 -0700

    credits screen

---
+++ Assets/Scripts/Utils/ScrollToBottom.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Utils {
+    public class ScrollToBottom : MonoBehaviour {
+        public float Speed = 0.01f;
+        public UnityEvent OnScrolledToBottom = new UnityEvent();
+
+        private ScrollRect scroller;
+        private bool atBottom;
+        private bool ready = false;
+
+        void Start() {
+            scroller = GetComponent<ScrollRect>();
+            scroller.normalizedPosition = new Vector2(1, 1);
+            StartCoroutine(ReadyUp());
+        }
+
+        void Update() {
+            if (scroller != null && !atBottom && ready) {
+                var offset = scroller.normalizedPosition;
+                offset.y -= Speed;
+                scroller.normalizedPosition = offset;
+                if (offset.y <= 0) {
+                    atBottom = true;
+                    OnScrolledToBottom.Invoke();
+                }
+            }
+        }
+
+        private IEnumerator ReadyUp() {
+            yield return new WaitForSeconds(2f);
+            ready = true;
+        }
+    }
+}
