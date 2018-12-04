75ef4fc3494f6fc503ca9bc396facf7d89d9ecbf
mwingfield <mwingfie@gmail.com>
2018-12-03 18:29:34 -0700

    fixed last min crane bugs

--- Assets/Scripts/Tooltip/RotateTooltipController.cs
+++ Assets/Scripts/Tooltip/RotateTooltipController.cs
@@ -6,7 +6,9 @@ public class RotateTooltipController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		foreach (var spriteRenderer in GetComponentsInChildren<SpriteRenderer>()) {
+			spriteRenderer.enabled = false;
+		}
 	}
 	
 	// Update is called once per frame
